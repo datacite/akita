291cd9c76def552c2842128b2fc76a92a6b27eb5
Martin Fenner <martin.fenner@datacite.org>
2020-05-27 19:01:56 +0200

    install apollo-boost

---
+++ hooks/withApollo.ts
@@ -0,0 +1,10 @@
+import withApollo from "next-with-apollo"
+import ApolloClient, { InMemoryCache } from "apollo-boost"
+ 
+export default withApollo(
+  ({ initialState }) =>
+    new ApolloClient({
+      uri: "https://api.datacite.org/graphql",
+      cache: new InMemoryCache().restore(initialState || {})
+    })
+)
