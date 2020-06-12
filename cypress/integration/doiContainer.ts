09e8ea6612e60af41c9224e53a727bb773bbbf8b
kjgarza <kgarza@datacite.org>
2020-06-12 12:04:34 +0200

    test: add cite-as
    https://github.com/datacite/akita/issues/5

--- cypress/integration/doiContainer.ts
+++ cypress/integration/doiContainer.ts
@@ -15,6 +15,13 @@ describe("DoiContainer", () => {
       .contains('DataCite XML')
       .should('be.visible')
   })
+
+  it("chage cite as", () => {
+    cy.get('select.cite-as')
+      .select('ieee')
+      // timeout for the query results to return
+      .get('.formatted-citation', { timeout: 10000 }).contains('C. Gallardo-Escárate, V. Valenzuela-Muñoz, G. Núñez-Acuña, and P. Haye, “Data from: SNP discovery and gene annotation in the surf clam Mesodesma donacium.” Dryad, 2014, doi: 10.5061/DRYAD.8JD18.')
+      })
 })
 
 export {}
