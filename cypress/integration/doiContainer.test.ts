092c947a098b3ad630de56b19593460cfeb41312
Martin Fenner <martin.fenner@datacite.org>
2020-07-05 18:31:44 +0200

    fix tests

--- cypress/integration/doiContainer.test.ts
+++ cypress/integration/doiContainer.test.ts
@@ -4,13 +4,13 @@ describe("DoiContainer", () => {
   })
 
   it("visit 10.70048/q3sn-h087", () => {
-    cy.get('h3.work',  { timeout: 10000 })
+    cy.get('h3.work', { timeout: 30000 })
       .contains('Chandra X-ray Observatory ObsId 1')
       .should('be.visible')
   })
 
   it("export box", () => {
-    cy.get('div#export-xml')
+    cy.get('div#export-xml', { timeout: 30000 })
       // timeout for the query results to return
       .contains('DataCite XML')
       .should('be.visible')
@@ -20,7 +20,7 @@ describe("DoiContainer", () => {
     cy.get('select.cite-as')
       .select('ieee')
       // timeout for the query results to return
-      .get('.formatted-citation', { timeout: 10000 })
+      .get('.formatted-citation', { timeout: 30000 })
       .should('be.visible')
       //.contains('CXC-DS, “Chandra X-ray Observatory ObsId 1.”')
       })
