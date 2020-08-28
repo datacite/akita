4c359c7403596b36f551eff46978e3dbb7c70bb5
Martin Fenner <martin.fenner@datacite.org>
2020-08-28 21:07:05 +0200

    remove broken code

--- src/components/WorkContainer/WorkContainer.tsx
+++ src/components/WorkContainer/WorkContainer.tsx
@@ -6,7 +6,6 @@ import { connectionFragment, contentFragment } from '../SearchWork/SearchWork'
 import { useQueryState } from 'next-usequerystate'
 import { Row, Col, Tab, Nav, NavItem } from 'react-bootstrap'
 import Pluralize from 'react-pluralize'
-import { NextSeo } from 'next-seo';
 import { compactNumbers } from '../../utils/helpers'
 import WorksListing from '../WorksListing/WorksListing'
 import Loading from '../Loading/Loading'
@@ -257,41 +256,9 @@ const WorkContainer: React.FunctionComponent<Props> = ({ item, searchQuery }) =>
   const content = () => {
     return (
       <React.Fragment>
-      <NextSeo
-      title="Using More of Config"
-      description="This example uses more of the available config options."
-      canonical="https://www.canonical.ie/"
-      openGraph={{
-        url: 'https://www.url.ie/a',
-        title: 'Open Graph Title',
-        description: 'Open Graph Description',
-        images: [
-          {
-            url: 'https://www.example.ie/og-image-01.jpg',
-            width: 800,
-            height: 600,
-            alt: 'Og Image Alt',
-          },
-          {
-            url: 'https://www.example.ie/og-image-02.jpg',
-            width: 900,
-            height: 800,
-            alt: 'Og Image Alt Second',
-          },
-          { url: 'https://www.example.ie/og-image-03.jpg' },
-          { url: 'https://www.example.ie/og-image-04.jpg' },
-        ],
-        site_name: 'SiteName',
-      }}
-      twitter={{
-        handle: '@handle',
-        site: '@site',
-        cardType: 'summary_large_image',
-      }}
-    />
-      <Col md={9} mdOffset={3} className="panel-list" id="content">
-        <Work doi={doi}></Work>
-      </Col>
+        <Col md={9} mdOffset={3} className="panel-list" id="content">
+          <Work doi={doi}></Work>
+        </Col>
       </React.Fragment>
     )
   }
