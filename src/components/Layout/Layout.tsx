66bf13b601c9cff532e531a94c128fd61168fb68
Martin Fenner <martin.fenner@datacite.org>
2020-09-01 08:29:52 +0200

    add feature flags. #111

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
+import { useFeature } from 'flagged'
+
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import Consent from '../Consent/Consent'
@@ -8,28 +10,32 @@ type Props = {
   path: string
 }
 
-const Layout: React.FunctionComponent<Props> = ({ children, path }) => (
-  <div>
-    <Head>
-      <title>DataCite Commons</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600"
-        rel="stylesheet"
-      />
-      <link
-        href="https://assets.datacite.org/stylesheets/doi.css?version=3.8.0"
-        rel="stylesheet"
-        type="text/css"
-      />
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-    </Head>
-    <Header path={path} />
-    <div className="container-fluid">{children}</div>
-    <Consent />
-    <Footer />
-  </div>
-)
+const Layout: React.FunctionComponent<Props> = ({ children, path }) => {
+  const showConsentCookie = useFeature('consentCookie')
+
+  return (
+    <div>
+      <Head>
+        <title>DataCite Commons</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600"
+          rel="stylesheet"
+        />
+        <link
+          href="https://assets.datacite.org/stylesheets/doi.css?version=3.8.0"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
+      <Header path={path} />
+      <div className="container-fluid">{children}</div>
+      {showConsentCookie && <Consent />}
+      <Footer />
+    </div>
+  )
+}
 
 export default Layout
