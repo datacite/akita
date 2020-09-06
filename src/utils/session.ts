38b71d1a4a3933ae6f763462311f19d65ffa391a
Martin Fenner <martin.fenner@datacite.org>
2020-09-06 07:19:19 +0200

    support single sign-on. #123

---
+++ src/utils/session.ts
@@ -0,0 +1,31 @@
+import { Cookies } from 'react-cookie-consent'
+import JsonWebToken from 'jsonwebtoken'
+
+export const session = () => {
+  let user = null
+  let jwt = null
+  let cert = null
+
+  const sessionCookie = Cookies.getJSON('_datacite')
+
+  if (sessionCookie && sessionCookie.authenticated) {
+    jwt = sessionCookie.authenticated.access_token
+
+    // RSA public key
+    cert = process.env.NEXT_PUBLIC_JWT_PUBLIC_KEY
+      ? process.env.NEXT_PUBLIC_JWT_PUBLIC_KEY.replace(/\\n/g, '\n')
+      : null
+  }
+
+  if (jwt && cert)
+    // verify asymmetric token, using RSA with SHA-256 hash algorithm
+    JsonWebToken.verify(jwt, cert, { algorithms: ['RS256'] }, function(error, payload) {
+      if (payload) {
+        user = payload
+      } else if (error) {
+        console.log('JWT verification error: ' + error.message)
+      }
+    })
+
+  return user
+}
