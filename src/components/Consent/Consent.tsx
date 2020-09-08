2374de02f752efcc34e86c2f42af1389e7134e38
Martin Fenner <martin.fenner@datacite.org>
2020-09-08 06:56:28 +0200

    set consent cookie for subdomain

--- src/components/Consent/Consent.tsx
+++ src/components/Consent/Consent.tsx
@@ -1,8 +1,14 @@
-
 import React from 'react'
 import CookieConsent from 'react-cookie-consent'
 
 const Consent = () => {
+  let domain = 'localhost'
+  if (process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_API_URL === 'https://api.datacite.org') {
+    domain = 'datacite.org'
+  } if (process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_API_URL === 'https://api.stage.datacite.org') {
+    domain = 'stage.datacite.org'
+  }
+
   return (
     <CookieConsent
       containerClasses="consent-cookie"
@@ -11,12 +17,12 @@ const Consent = () => {
       declineButtonText="I decline"
       sameSite="strict"
       cookieName="_consent"
-      // extraCookieOptions={{ domain: "datacite.org" }}
+      extraCookieOptions={{ domain: domain }}
       overlay={true}
       enableDeclineButton
     >
-      This website uses cookies to enable important site functionality
-      including analytics and personalization.{' '}
+      This website uses cookies to enable important site functionality including
+      analytics and personalization.{' '}
       <a
         href="https://datacite.org/privacy.html"
         target="_blank"
