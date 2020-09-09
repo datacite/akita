293123d5fdd7e62eb0588c504a8eb4e8ac5bf905
Martin Fenner <martin.fenner@datacite.org>
2020-09-09 21:38:20 +0200

    add google analytics. #108

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -12,6 +12,7 @@ type Props = {
 
 const Layout: React.FunctionComponent<Props> = ({ children, path }) => {
   const showConsentCookie = useFeature('consentCookie')
+  const trackingId = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 
   return (
     <div>
@@ -23,13 +24,30 @@ const Layout: React.FunctionComponent<Props> = ({ children, path }) => {
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600"
           rel="stylesheet"
         />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jpswalsh/academicons@1/css/academicons.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/jpswalsh/academicons@1/css/academicons.min.css"
+        />
         <link
           href="https://datacite.org/stylesheets/doi.css?version=3.8.0"
           rel="stylesheet"
           type="text/css"
         />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <script
+          async
+          src={'https://www.googletagmanager.com/gtag/js?id=' + trackingId}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${trackingId}');
+            `
+          }}
+        />
       </Head>
       <Header path={path} />
       <div className="container-fluid">{children}</div>
