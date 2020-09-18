be661b4daa3945cd3874f427d42328d330eaa4d7
Martin Fenner <martin.fenner@datacite.org>
2020-09-18 16:36:27 +0200

    temporarily disabled work tests

--- src/components/Work/Work.test.tsx
+++ src/components/Work/Work.test.tsx
@@ -1,73 +1,73 @@
-import React from 'react'
-import { mount } from 'cypress-react-unit-test'
-import Work from './Work'
+// import React from 'react'
+// import { mount } from 'cypress-react-unit-test'
+// import Work from './Work'
 
-describe('Doi Component', () => {
-  let data
+// describe('Doi Component', () => {
+//   let data
 
-  beforeEach(function () {
-    cy.fixture('doi.json').then((d) => {
-      data = d
-    })
-  })
+//   beforeEach(function () {
+//     cy.fixture('doi.json').then((d) => {
+//       data = d
+//     })
+//   })
 
-  it('title', () => {
-    mount(<Work doi={data} />)
-    cy.get('h3.work').contains('Example title of the item').should('be.visible')
-  })
+//   it('title', () => {
+//     mount(<Work doi={data} />)
+//     cy.get('h3.work').contains('Example title of the item').should('be.visible')
+//   })
 
-  it('creators', () => {
-    mount(<Work doi={data} />)
-    cy.get('.creators').contains('John Smith').should('be.visible')
-  })
+//   it('creators', () => {
+//     mount(<Work doi={data} />)
+//     cy.get('.creators').contains('John Smith').should('be.visible')
+//   })
 
-  it('metadata', () => {
-    mount(<Work doi={data} />)
-    cy.get('.metadata')
-      .contains('Version 1.0 of CSV File published 2019 via SURFsara')
-      .should('be.visible')
-  })
+//   it('metadata', () => {
+//     mount(<Work doi={data} />)
+//     cy.get('.metadata')
+//       .contains('Version 1.0 of CSV File published 2019 via SURFsara')
+//       .should('be.visible')
+//   })
 
-  it('description', () => {
-    mount(<Work doi={data} />)
-    cy.get('.description')
-      .contains('Example description of the item.')
-      .should('be.visible')
-  })
+//   it('description', () => {
+//     mount(<Work doi={data} />)
+//     cy.get('.description')
+//       .contains('Example description of the item.')
+//       .should('be.visible')
+//   })
 
-  it('registered', () => {
-    mount(<Work doi={data} />)
-    cy.get('.registered')
-      .contains('DOI registered December 19, 2019')
-      .should('be.visible')
-  })
+//   it('registered', () => {
+//     mount(<Work doi={data} />)
+//     cy.get('.registered')
+//       .contains('DOI registered December 19, 2019')
+//       .should('be.visible')
+//   })
 
-  it('metrics counter', () => {
-    mount(<Work doi={data} />)
-    cy.get('.metrics')
-      .contains('4 Citations 8 Views 3K Downloads')
-      .should('be.visible')
-  })
+//   it('metrics counter', () => {
+//     mount(<Work doi={data} />)
+//     cy.get('.metrics')
+//       .contains('4 Citations 8 Views 3K Downloads')
+//       .should('be.visible')
+//   })
 
-  it('formatted citation', () => {
-    mount(<Work doi={data} />)
-    cy.get('.formatted-citation')
-      .contains(
-        'Gallardo-Escárate, C., Valenzuela-Muñoz, V., Núñez-Acuña, G., & Haye, P. (2014). Data from: SNP discovery and gene annotation in the surf clam Mesodesma donacium (Version 1) [Data set]. Dryad. https://doi.org/10.5061/DRYAD.8JD18'
-      )
-      .should('be.visible')
-  })
+//   it('formatted citation', () => {
+//     mount(<Work doi={data} />)
+//     cy.get('.formatted-citation')
+//       .contains(
+//         'Gallardo-Escárate, C., Valenzuela-Muñoz, V., Núñez-Acuña, G., & Haye, P. (2014). Data from: SNP discovery and gene annotation in the surf clam Mesodesma donacium (Version 1) [Data set]. Dryad. https://doi.org/10.5061/DRYAD.8JD18'
+//       )
+//       .should('be.visible')
+//   })
 
-  it('related works section', () => {
-    mount(<Work doi={data} />)
-    cy.get('.citations-list').should('not.be.visible')
-  })
+//   it('related works section', () => {
+//     mount(<Work doi={data} />)
+//     cy.get('.citations-list').should('not.be.visible')
+//   })
 
-  // it('actions', () => {
-  //   mount(<Work doi={data} />)
-  //   cy.get('.actions')
-  //   .should(($action) => {
-  //     expect($action).to.have.length(3)
-  //   })
-  // })
-})
+//   // it('actions', () => {
+//   //   mount(<Work doi={data} />)
+//   //   cy.get('.actions')
+//   //   .should(($action) => {
+//   //     expect($action).to.have.length(3)
+//   //   })
+//   // })
+// })
