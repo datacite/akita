972f1fe8774a149d7b2f979b476f63bb74ef09e0
Martin Fenner <martin.fenner@datacite.org>
2020-10-17 13:26:14 +0200

    test server errors

--- cypress/integration/server.test.ts
+++ cypress/integration/server.test.ts
@@ -1,44 +1,44 @@
-// describe("Server error", () => {  
-//   beforeEach(() => {
-//     cy.setCookie('_consent', 'true')
-//     cy.route2('POST', '**/graphql', {
-//       "errors": [
-//         {
-//           "message": "Internal Server Error"
-//         }
-//       ]
-//     })a
-//   })
+describe("Server error", () => {  
+  beforeEach(() => {
+    cy.setCookie('_consent', 'true')
+    cy.route2('POST', '**/graphql', {
+      "errors": [
+        {
+          "message": "Internal Server Error"
+        }
+      ]
+    })
+  })
 
-//   it('search works', () => {
-//     cy.visit('/doi.org')
-//     cy.get('input[name="query"]')
-//       .type('hallett{enter}')
-//       .get('.alert > h4')
-//       .should('contain', 'An error occured.')
-//       .get('.alert > p')
-//       .should('contain', 'Internal Server Error')
-//   })
+  it('search works', () => {
+    cy.visit('/doi.org')
+    cy.get('input[name="query"]')
+      .type('hallett{enter}')
+      .get('.alert > h4')
+      .should('contain', 'An error occured.')
+      // .get('.alert > p')
+      // .should('contain', 'Internal Server Error')
+  })
 
-//   // it('search people', () => {
-//   //   cy.visit('/orcid.org')
-//   //   cy.get('input[name="query"]')
-//   //     .type('hallett{enter}')
-//   //     .get('.alert > h4')
-//   //     .should('contain', 'An error occured.')
-//   //     .get('.alert > p')
-//   //     .should('contain', 'Internal Server Error')
-//   // })
+  it('search people', () => {
+    cy.visit('/orcid.org')
+    cy.get('input[name="query"]')
+      .type('hallett{enter}')
+      .get('.alert > h4')
+      .should('contain', 'An error occured.')
+      // .get('.alert > p')
+      // .should('contain', 'Internal Server Error')
+  })
 
-//   // it('search organizations', () => {
-//   //   cy.visit('/ror.org')
-//   //   cy.get('input[name="query"]')
-//   //     .type('cambridge{enter}')
-//   //     .get('.alert > h4')
-//   //     .should('contain', 'An error occured.')
-//   //     .get('.alert > p')
-//   //     .should('contain', 'Internal Server Error')
-//   // })
-// })
+  it('search organizations', () => {
+    cy.visit('/ror.org')
+    cy.get('input[name="query"]')
+      .type('cambridge{enter}')
+      .get('.alert > h4')
+      .should('contain', 'An error occured.')
+      // .get('.alert > p')
+      // .should('contain', 'Internal Server Error')
+  })
+})
 
 export {}
