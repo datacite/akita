a3db2c98608145275bfcf1702d91d9332b4ae2ae
Martin Fenner <martin.fenner@datacite.org>
2020-10-23 18:51:28 +0200

    don't use devtools sourcemap. #143

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 // Use the hidden-source-map option when you don't want the source maps to be
 // publicly available on the servers, only to the error reporting
-const withSourceMaps = require('@zeit/next-source-maps')
+const withSourceMaps = require('@zeit/next-source-maps')({
+  devtool: 'hidden-source-map'
+})
 
 // Use the SentryWebpack plugin to upload the source maps during build step
 const SentryWebpackPlugin = require('@sentry/webpack-plugin')
