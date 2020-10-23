de7ddcf4ef6cc02c399cfe0684adfe35f17cff65
Martin Fenner <martin.fenner@datacite.org>
2020-10-23 18:23:36 +0200

    standard next.config #143

--- next.config.js
+++ next.config.js
@@ -67,13 +67,5 @@ module.exports = withSourceMaps({
     }
 
     return config
-  },
-  distDir: "_next",
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
+  }
 })
