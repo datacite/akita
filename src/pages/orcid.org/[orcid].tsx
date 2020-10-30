9d69de020f91939342b31729c89574e1beddd87d
Martin Fenner <martin.fenner@datacite.org>
2020-10-30 07:36:11 +0100

    fix plural

--- src/pages/orcid.org/[orcid].tsx
+++ src/pages/orcid.org/[orcid].tsx
@@ -272,7 +272,7 @@ const PersonPage: React.FunctionComponent<Props> = ({
       <>
         <Col md={9} mdOffset={3}>
           {totalCount > 0 && (
-            <h3 className="member-results">{pluralize(totalCount, 'Works')}</h3>
+            <h3 className="member-results">{pluralize(totalCount, 'Work')}</h3>
           )}
         </Col>
         <WorksListing
