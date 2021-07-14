a9356bca5057335cf5a2f04ac15c22ed6bb2d564
Martin Fenner <martin.fenner@datacite.org>
2021-07-14 18:34:50 +0200

    Update userMenu.test.ts

--- cypress/integration/userMenu.test.ts
+++ cypress/integration/userMenu.test.ts
@@ -1,45 +1,45 @@
 /// <reference types="cypress" />
 
-// describe('User Menu', () => {
-//   beforeEach(() => {
-//     cy.setCookie('_consent', 'true')
-//     cy.setCookie('_datacite', Cypress.env('userCookie'), { log: false })
-//   })
+describe('User Menu', () => {
+  beforeEach(() => {
+    cy.setCookie('_consent', 'true')
+    cy.setCookie('_datacite', Cypress.env('userCookie'), { log: false })
+  })
 
-//   it('menu item', () => {
-//     cy.visit('/')
-//     cy.get('#sign-in', { timeout: 30000 }).contains('Martin Fenner')
-//   })
+  it('menu item', () => {
+    cy.visit('/')
+    cy.get('#sign-in', { timeout: 30000 }).contains('Martin Fenner')
+  })
 
-//   it('beta tester', () => {
-//     cy.visit('/')
-//     cy.get('#sign-in', { timeout: 30000 }).click()
-//     cy.get('[data-cy=beta]').contains('Beta Tester')
-//   })
+  // it('beta tester', () => {
+  //   cy.visit('/')
+  //   cy.get('#sign-in', { timeout: 30000 }).click()
+  //   cy.get('[data-cy=beta]').contains('Beta Tester')
+  // })
 
-//   it('settings', () => {
-//     cy.visit('/')
-//     cy.get('#sign-in', { timeout: 30000 }).click()
-//     cy.get('[data-cy=settings]').contains('Settings')
-//   })
+  it('settings', () => {
+    cy.visit('/')
+    cy.get('#sign-in', { timeout: 30000 }).click()
+    cy.get('[data-cy=settings]').contains('Settings')
+  })
 
-//   it('commons page', () => {
-//     cy.visit('/')
-//     cy.get('#sign-in', { timeout: 30000 }).click()
-//     cy.get('[data-cy=commons-page]').contains('Commons Page')
-//   })
+  it('commons page', () => {
+    cy.visit('/')
+    cy.get('#sign-in', { timeout: 30000 }).click()
+    cy.get('[data-cy=commons-page]').contains('Commons Page')
+  })
 
-//   it('orcid', () => {
-//     cy.visit('/')
-//     cy.get('#sign-in', { timeout: 30000 }).click()
-//     cy.get('[data-cy=orcid]').contains('ORCID Record')
-//   })
+  it('orcid', () => {
+    cy.visit('/')
+    cy.get('#sign-in', { timeout: 30000 }).click()
+    cy.get('[data-cy=orcid]').contains('ORCID Record')
+  })
 
-//   it('sign out', () => {
-//     cy.visit('/')
-//     cy.get('#sign-in', { timeout: 30000 }).click()
-//     cy.get('[data-cy=signout]').contains('Sign Out')
-//   })
-// })
+  it('sign out', () => {
+    cy.visit('/')
+    cy.get('#sign-in', { timeout: 30000 }).click()
+    cy.get('[data-cy=signout]').contains('Sign Out')
+  })
+})
 
 export {}
