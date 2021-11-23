895fa8bbfb2ce8e03973b18ecdcd88cd7a6c8dd1
jrhoads <jrhoads@datacite.org>
2021-11-23 15:23:43 -0500

    Remove Docker files and update README.  Docker not used for deployment.

--- vendor/docker/20_next_build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/sbin/setuser app yarn build
--- vendor/docker/70_templates.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-dockerize -template /home/app/webapp/vendor/docker/robots.txt.tmpl:/home/app/webapp/public/robots.txt
-dockerize -template /home/app/webapp/vendor/docker/webapp.conf.tmpl:/etc/nginx/sites-enabled/webapp.conf
