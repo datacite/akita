5d072be0f5f082948526922dc904291bdf8c7e88
Bryceson Laing <bryceson.laing@datacite.org>
2023-02-21 12:06:20 +0000

    fair vis 6 - Updating chart spec

--- src/components/HorizontalStackedBarChart/HorizontalStackedBarChart.tsx
+++ src/components/HorizontalStackedBarChart/HorizontalStackedBarChart.tsx
@@ -24,6 +24,365 @@ const actions = {
   editor: false
 }
 
+const DATA = [
+  {
+  "Name": "ARDC",
+  "count": 1
+  },
+  {
+  "Name": "Arizona State University",
+  "count": 1
+  },
+  {
+  "Name": "arolsen-archives",
+  "count": 1
+  },
+  {
+  "Name": "Atmospheric Model Data",
+  "count": 1
+  },
+  {
+  "Name": "British Antarctic Survey",
+  "count": 1
+  },
+  {
+  "Name": "Brunel University London",
+  "count": 1
+  },
+  {
+  "Name": "Center for Open Science",
+  "count": 1
+  },
+  {
+  "Name": "Centre de coopération internationale en recherche agronomique pour le développement",
+  "count": 1
+  },
+  {
+  "Name": "Centre for Biodiversity Genomics",
+  "count": 1
+  },
+  {
+  "Name": "Centro Euro-Mediterraneo sui Cambiamenti Climatici (CMCC) Foundation",
+  "count": 1
+  },
+  {
+  "Name": "Colorado School of Mines",
+  "count": 1
+  },
+  {
+  "Name": "Crossref",
+  "count": 1
+  },
+  {
+  "Name": "Curtin University",
+  "count": 1
+  },
+  {
+  "Name": "DARA bucket",
+  "count": 1
+  },
+  {
+  "Name": "Data Archiving and Networked Services",
+  "count": 1
+  },
+  {
+  "Name": "DKRZ",
+  "count": 1
+  },
+  {
+  "Name": "ecohealthalliance.org",
+  "count": 1
+  },
+  {
+  "Name": "Elsevier",
+  "count": 1
+  },
+  {
+  "Name": "embl-ebi",
+  "count": 1
+  },
+  {
+  "Name": "eScire",
+  "count": 1
+  },
+  {
+  "Name": "European Synchrotron Radiation Facility",
+  "count": 1
+  },
+  {
+  "Name": "Exaptive",
+  "count": 1
+  },
+  {
+  "Name": "Food and Agriculture Organization of the United Nations",
+  "count": 1
+  },
+  {
+  "Name": "github",
+  "count": 1
+  },
+  {
+  "Name": "GreyNet International",
+  "count": 1
+  },
+  {
+  "Name": "Helmholtz Centre Potsdam GFZ German Research Centre for Geosciences",
+  "count": 1
+  },
+  {
+  "Name": "IISKS",
+  "count": 1
+  },
+  {
+  "Name": "Jet Propulsion Lab",
+  "count": 1
+  },
+  {
+  "Name": "KU Leuven",
+  "count": 1
+  },
+  {
+  "Name": "lens",
+  "count": 1
+  },
+  {
+  "Name": "Macquarie University",
+  "count": 1
+  },
+  {
+  "Name": "Manaaki Whenua - Landcare Research",
+  "count": 1
+  },
+  {
+  "Name": "METADATAWORKS",
+  "count": 1
+  },
+  {
+  "Name": "MPG",
+  "count": 1
+  },
+  {
+  "Name": "MPI",
+  "count": 1
+  },
+  {
+  "Name": "National Institute for Materials Science",
+  "count": 1
+  },
+  {
+  "Name": "National Research Council Canada",
+  "count": 1
+  },
+  {
+  "Name": "New Digital Research Infrastructure Organization (NDRIO)",
+  "count": 1
+  },
+  {
+  "Name": "New Zealand eScience Infrastructure",
+  "count": 1
+  },
+  {
+  "Name": "northwestern university",
+  "count": 1
+  },
+  {
+  "Name": "ntu",
+  "count": 1
+  },
+  {
+  "Name": "Ocean Networks Canada Society",
+  "count": 1
+  },
+  {
+  "Name": "ORCID",
+  "count": 1
+  },
+  {
+  "Name": "ourresearch",
+  "count": 1
+  },
+  {
+  "Name": "Oxford University Library Service Databank",
+  "count": 1
+  },
+  {
+  "Name": "Parafia Ewangelicko-Augsburska w Gdańsku",
+  "count": 1
+  },
+  {
+  "Name": "Princeton University",
+  "count": 1
+  },
+  {
+  "Name": "Queen's University Belfast",
+  "count": 1
+  },
+  {
+  "Name": "sages",
+  "count": 1
+  },
+  {
+  "Name": "Stanford University Libraries",
+  "count": 1
+  },
+  {
+  "Name": "The Danish National Archives",
+  "count": 1
+  },
+  {
+  "Name": "TU Delft",
+  "count": 1
+  },
+  {
+  "Name": "Umeå universitet",
+  "count": 1
+  },
+  {
+  "Name": "univ-grenoble-alpes",
+  "count": 1
+  },
+  {
+  "Name": "Universidade Fernando Pessoa",
+  "count": 1
+  },
+  {
+  "Name": "University Kiel",
+  "count": 1
+  },
+  {
+  "Name": "University of Arizona",
+  "count": 1
+  },
+  {
+  "Name": "University of British Columbia",
+  "count": 1
+  },
+  {
+  "Name": "University of Cape Town",
+  "count": 1
+  },
+  {
+  "Name": "University of Sheffield",
+  "count": 1
+  },
+  {
+  "Name": "US Department of Energy (DOE), Office of Scientific and Technical Information (OSTI)",
+  "count": 1
+  },
+  {
+  "Name": "Wageningen University & Research",
+  "count": 1
+  },
+  {
+  "Name": "wsl",
+  "count": 1
+  },
+  {
+  "Name": "Zürcher Hochschule der Künste",
+  "count": 1
+  },
+  {
+  "Name": "American Geophysical Union",
+  "count": 2
+  },
+  {
+  "Name": "BL Repository Services",
+  "count": 2
+  },
+  {
+  "Name": "British Library",
+  "count": 2
+  },
+  {
+  "Name": "DataOne",
+  "count": 2
+  },
+  {
+  "Name": "German National Library of Science and Technology",
+  "count": 2
+  },
+  {
+  "Name": "Karlsruhe Institute of Technology",
+  "count": 2
+  },
+  {
+  "Name": "Pacific northwest National Laboratory",
+  "count": 2
+  },
+  {
+  "Name": "Technical University of Denmark",
+  "count": 2
+  },
+  {
+  "Name": "The Australian National University",
+  "count": 2
+  },
+  {
+  "Name": "unipd",
+  "count": 2
+  },
+  {
+  "Name": "University of Ottawa",
+  "count": 2
+  },
+  {
+  "Name": "University of Virginia",
+  "count": 2
+  },
+  {
+  "Name": "berkley lab",
+  "count": 3
+  },
+  {
+  "Name": "California Digital Library",
+  "count": 3
+  },
+  {
+  "Name": "Caltech",
+  "count": 3
+  },
+  {
+  "Name": "Imperial College London",
+  "count": 3
+  },
+  {
+  "Name": "Personal",
+  "count": 3
+  },
+  {
+  "Name": "usda",
+  "count": 3
+  },
+  {
+  "Name": "ZHB Luzern",
+  "count": 3
+  },
+  {
+  "Name": "National Institute of Standards and Technology",
+  "count": 4
+  },
+  {
+  "Name": "Spanish National Research Council (CSIC)",
+  "count": 4
+  },
+  {
+  "Name": "TIB",
+  "count": 4
+  },
+  {
+  "Name": "metadata game changers",
+  "count": 5
+  },
+  {
+  "Name": "Lyrasis",
+  "count": 6
+  },
+  {
+  "Name": "TU Wien",
+  "count": 9
+  }
+  ]
+
 const VerticalBarChart: React.FunctionComponent<Props> = ({
   title,
   data,
@@ -150,18 +509,12 @@ const VerticalBarChart: React.FunctionComponent<Props> = ({
   }
 
   const stackedBarChartSpec: VisualizationSpec = {
-    $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
     data: {
-      name: 'table'
+        url: 'https://gist.githubusercontent.com/kjgarza/7d6afa241d2d39d5a0a9b4add8a17bdb/raw/0c423a00e28e51a9b494886fcebf88d18b8a7568/face-domain-name.json'
     },
-    padding: { left: 5, top: 5, right: 5, bottom: 5 },
-    axes: [
-      { orient: "bottom", scale: "xscale", tickCount: 5 },
-      { orient: "left", scale: "yscale", tickCount: 5 }
-    ],
+    width: 500,
     mark: {
       type: "bar",
-      cursor: "pointer",
       tooltip: true
     },
     transform: [
@@ -169,46 +522,27 @@ const VerticalBarChart: React.FunctionComponent<Props> = ({
         calculate: "datum.count * 100",
         as: "percentage"
       },
-      {calculate: "datum."+'variable'+" == '__missing__'? '__missing__' : 'complete'",
-       as: "completenes"
-       }
+      // {calculate: "datum.Name == '__missing__'? '__missing__' : 'complete'",
+      //  as: "completenes"
+      //  }
       ],
-    selection: {
-      highlight: {
-        type: "single",
-        empty: "none",
-        on: "mouseover"
-      }
-    },
-    width: chartWidth,
-    height: 200,
     encoding: {
-      x: {
-          "aggregate": "sum",
-          "field": "count",
-          "stack": "normalize",
-          "type": "quantitative",
-          "axis": {"format": ".0%"},
-          "title": 'variable'
-      },
-      color: {
-          "field": "completenes"
-      },
-      // order: {
-      //     "aggregate": "sum",
-      //     "sort": "descending",
-      //     "field": "count"
-      // }
-  },
-    config: {
-      view: {
-        stroke: null
-      },
-      axis: {
-        grid: false,
-        title: null,
-        labelFlush: false
-      }
+        x: {
+            aggregate: "sum",
+            field: "count",
+            stack: "normalize",
+            type: "quantitative",
+            axis: {"format": ".0%"},
+            title: 'Name'
+        },
+        color: {
+            field: "Name"
+        },
+        // order: {
+        //     "aggregate": "sum",
+        //     "sort": "descending",
+        //     "field": "count"
+        // }
     }
   }
 
@@ -230,7 +564,7 @@ const VerticalBarChart: React.FunctionComponent<Props> = ({
         <VegaLite
           renderer="svg"
           spec={stackedBarChartSpec}
-          data={{ table: mydata.slice(0, 10) }}
+          // data={{ table: mydata.slice(0, 10) }}
           actions={actions}
         />
       </div>
