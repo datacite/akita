c148ac062f290918354ac85e746ba096a0d187d2
Richard Hallett <richard@richardhallett.com>
2023-09-19 11:37:30 +0200

    Fix warnings due to incorrect html

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,13 +14,14 @@ const IndexPage = () => {
         <ExampleText>
           <p>
             Search works by keyword(s) or DOI.<br /><br />
-
+          </p>
             Examples:
             <ul>
               <li><a href="/doi.org?query=climate+change">climate change</a></li>
               <li><a href="/doi.org?query=10.14454%2F3w3z-sa82">10.14454/3w3z-sa82</a></li>
             </ul>
 
+          <p>
             Documentation is available in <a href="https://support.datacite.org/docs/datacite-commons" target="_blank" rel="noreferrer">DataCite Support.</a>
           </p>
         </ExampleText>
