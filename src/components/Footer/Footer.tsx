f21e90e86e6816d218bbaa5cb596b63f77737bd8
Kelly Stathis <kelly.a.stathis@gmail.com>
2023-09-20 12:49:45 -0700

    update twitter icon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -131,7 +131,7 @@ const Footer = () => {
               <FontAwesomeIcon icon={faGithub} />
             </a>
             <a href="https://twitter.com/datacite" className="share">
-              <FontAwesomeIcon icon={faTwitter} />
+              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" style="fill: #a3acb2;position: relative;bottom: -3;"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path></svg>
             </a>
             <a href="https://openbiblio.social/@datacite" className="share">
               <FontAwesomeIcon icon={faMastodon} />
