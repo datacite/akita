2bf2ba9b8f3569c896e4bd9262ef7fa3817e73c1
jrhoads <jrhoads@datacite.org>
2023-09-21 11:46:24 -0400

    _consent cookie should exist

--- cypress/integration/consent.test.ts
+++ cypress/integration/consent.test.ts
@@ -17,9 +17,8 @@ describe('Consent', () => {
       cy.get('.CookieConsent', { timeout: 30000 }).contains(
         'We use cookies on our website. Some are technically necessary, others help us improve your user experience.'
       )
-      cy.get('#rcc-confirm-button').should("exist")
       cy.get('#rcc-confirm-button').click()
-      cy.wait(1000)
+      cy.getCookie('_consent').should('exist')
       cy.getCookie('_consent').should('have.property', 'value', 'true')
       cy.get('.navbar-brand .commons-logo', { timeout: 30000 }).should('have.attr', 'src').should('include','commons-logo.svg')
     })
