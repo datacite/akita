dc3fa211eb3b769903c0c0c15aef0095b602bf26
Bryceson Laing <bryceson.laing@datacite.org>
2024-02-27 11:04:59 +0000

    Ignore hydration errors because Cypress and React Hydrating don't get along

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -12,4 +12,20 @@
   //return false
 //})
 
+
+// https://medium.com/@edweng/ign-the-cypress-remix-hydration-error-b0bc256d853a
+Cypress.on("uncaught:exception", (err) => {
+  // Cypress and React Hydrating the document don't get along
+  // for some unknown reason. Hopefully we figure out why eventually
+  // so we can remove this.
+  if (
+    /hydrat/i.test(err.message) ||
+    /Minified React error #418/.test(err.message) ||
+    /Minified React error #419/.test(err.message) ||
+    /Minified React error #423/.test(err.message)
+  ) {
+    return false;
+  }
+});
+
 export {}
