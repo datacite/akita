0aaabdc341e3da2e29c4bd86cf99dd4b9aa55b09
jrhoads <jrhoads@datacite.org>
2024-06-19 03:39:09 -0400

    Formatting python api

--- api/app.py
+++ api/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, jsonify
+import os
+
 from datacitekit.extractors import extract_doi
 from datacitekit.related_works import get_full_corpus_doi_attributes
 from datacitekit.reports import RelatedWorkReports
-import os
+from flask import Flask, jsonify
 
 DOI_API = os.getenv("DATACITE_API_URL", "https://api.stage.datacite.org/dois/")
 app = Flask(__name__)
+
 
 @app.route("/api/doi/related-graph/<path:doi>", methods=["GET"])
 def related_works(doi):
@@ -18,13 +20,11 @@ def related_works(doi):
         return jsonify({"error": "DOI not found"}), 404
     report = RelatedWorkReports(full_doi_attributes)
 
-    graph = {
-        "nodes": report.aggregate_counts,
-        "links": report.type_connection_report
-    }
+    non_zero_nodes = [agg for agg in report.aggregate_counts if agg["count"] > 0]
+    graph = {"nodes": non_zero_nodes, "links": report.type_connection_report}
 
     return jsonify(graph)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
