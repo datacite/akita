f76bbabeaac484b66197ea2ea74ce9bb96620437
Bryceson Laing <bryceson.laing@datacite.org>
2024-06-21 17:27:54 +0100

    Fixed JSON parse error

--- src/utils/apolloClient/apolloClient.ts
+++ src/utils/apolloClient/apolloClient.ts
@@ -2,7 +2,7 @@ import { cookies } from 'next/headers'
 import apolloClientBuilder from './builder'
 
 export function getAuthToken() {
-  const sessionCookie = JSON.parse((cookies().get('_datacite') as any).value)
+  const sessionCookie = JSON.parse((cookies().get('_datacite') as any)?.value || '{}')
   return sessionCookie &&
     sessionCookie.authenticated &&
     sessionCookie.authenticated.access_token
