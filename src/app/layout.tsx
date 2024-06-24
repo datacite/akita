2f22ff8fe7b5f4e82cc2d00f598f77158dfe8132
Bryceson Laing <bryceson.laing@datacite.org>
2024-06-24 15:04:31 +0100

    Migrated header to bootstrap-4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import PlausibleProvider from 'next-plausible'
 import { Source_Sans_3 } from 'next/font/google';
 import * as Sentry from '@sentry/node'
 
-import 'maltipoo/assets/stylesheets/doi.min.css'
 import '../styles.css'
 
 // properly handle fontawesome icons
