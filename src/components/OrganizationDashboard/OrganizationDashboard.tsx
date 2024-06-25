c2f5094499c436e183307dad8e5fdac77336719e
jrhoads <jrhoads@datacite.org>
2024-06-25 09:49:03 -0400

    Remove ForceDirectedGraph from orgs

--- src/components/OrganizationDashboard/OrganizationDashboard.tsx
+++ src/components/OrganizationDashboard/OrganizationDashboard.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
-import { gql, useQuery } from '@apollo/client'
-
 import { Works } from 'src/data/types'
-import { ForceDirectedGraphLink, ForceDirectedGraphNode } from '../ForceDirectedGraph/ForceDirectedGraph'
 import WorksDashboard from '../WorksDashboard/WorksDashboard'
-import { resourceTypeDomain } from '../../data/color_palettes'
 
 type Props = {
   rorId?: string
@@ -19,106 +15,9 @@ export interface ContentFacet {
   count: number
 }
 
-const FORCE_DIRECTED_GRAPH_GQL = gql`
-  query forcedDirectedGraphQuery(
-    $id: ID
-    $gridId: ID
-    $crossrefFunderId: ID
-    $referenceOrganizationId: String
-  ) {
-    organization(
-      id: $id
-      gridId: $gridId
-      crossrefFunderId: $crossrefFunderId
-    ) {
-      name
-      works(first: 100) {
-        totalCount
-        nodes {
-          id
-          types {
-            resourceTypeGeneral
-            resourceType
-          }
-          titles {
-            title
-          }
-          references(organizationId: $referenceOrganizationId) {
-            totalCount
-            nodes {
-              id
-            }
-          }
-        }
-      }
-    }
-  }
-`
-
-interface ForceDirectedOrganizationType {
-  works: Works
-}
-
-interface ForceDirectedQueryData {
-  organization: ForceDirectedOrganizationType
-}
-
-interface ForceDirectedQueryVar {
-  id?: string
-  gridId?: string
-  crossrefFunderId?: string
-}
-
 const OrganizationDashboard: React.FunctionComponent<Props> = ({
   works,
-  rorId,
-  gridId,
-  crossrefFunderId
 }) => {
-  const { loading, data } = useQuery<
-    ForceDirectedQueryData,
-    ForceDirectedQueryVar
-  >(FORCE_DIRECTED_GRAPH_GQL, {
-    errorPolicy: 'all',
-    variables: {
-      id: rorId,
-      gridId: gridId,
-      crossrefFunderId: crossrefFunderId
-    }
-  })
-
-  // Links between nodes
-  // Authors -> dois
-  // Dois -> dois
-  
-  const forceDirectedWorks = loading ? [] : data?.organization.works.nodes || []
-  const nodes: ForceDirectedGraphNode[] = []
-  const links: ForceDirectedGraphLink[] = []
-  forceDirectedWorks.forEach((work, index) => {
-    const title = work.titles[0] && work.titles[0].title ? work.titles[0].title : 'Unknown'
-
-    const type = work.types && work.types.resourceTypeGeneral ?
-    resourceTypeDomain.find(d => d === work.types.resourceTypeGeneral) ? work.types.resourceTypeGeneral : 'Other'
-      : 'Missing'
-
-    const node = { name: title, group: type}
-    nodes.push(node)
-
-    links.concat(work.references?.nodes.map(ref => {
-      const relatedWork = forceDirectedWorks.findIndex(w => w.id === ref.id)
-      return { source: index, target: relatedWork, value: 1 }
-    }) || [])
-
-
-
-    // TESTING RANDOM LINKS
-    for (let i = 0; i < Math.floor(Math.random() * 2); i++) {
-      const randomLink = { source: index, target: Math.floor(Math.random() * forceDirectedWorks.length), value: 1 }
-      links.push(randomLink)
-    }
-  })
-
-  
   return (
     <WorksDashboard works={works}>
       {/* <ForceDirectedGraph
