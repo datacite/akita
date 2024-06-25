bffff6aed702b9b8cc6c83206c9fec0f58ef2a67
Bryceson Laing <bryceson.laing@datacite.org>
2024-06-25 10:36:49 +0100

    Updated WorkListing to Bootstrap 4

---
+++ src/components/Layout-4/Layout.test.tsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+// import { mount } from '@cypress/react'
+// import Layout from './Layout'
+
+// describe('Layout Component', () => {
+//   it('title', () => {
+//     mount(<Layout path={'/doi.org'} />)
+//     cy.get('.navbar-brand').contains('DataCite Commons').should('be.visible')
+//   })
+
+//   it('children', () => {
+//     const children = 'Bla bla'
+//     mount(<Layout path={'/doi.org'} >{children}</Layout>)
+//     cy.get('.container-fluid').contains('Bla bla').should('be.visible')
+//   })
+// })
+
+export {}
---
+++ src/components/Layout-4/index.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import React from 'react'
+import RBContainer, { ContainerProps } from 'react-bootstrap-4/Container'
+import RBRow, { RowProps } from 'react-bootstrap-4/Row'
+import RBCol, { ColProps } from 'react-bootstrap-4/Col'
+import RBAlert, { AlertProps } from 'react-bootstrap-4/Alert'
+
+export function Grid(props: ContainerProps) {
+  return <RBContainer {...props}>{props.children}</RBContainer>
+}
+
+export function Row(props: RowProps) {
+  return <RBRow {...props}>{props.children}</RBRow>
+}
+
+export function Col(props: ColProps) {
+  return <RBCol {...props}>{props.children}</RBCol>
+}
+
+export function Alert(props: AlertProps) {
+  return <RBAlert {...props}>{props.children}</RBAlert>
+}
