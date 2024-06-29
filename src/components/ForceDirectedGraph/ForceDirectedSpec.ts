58bab6d0bad5266f1a05aa116869e978853456fe
jrhoads <jrhoads@datacite.org>
2024-06-29 22:56:26 +0200

    Move ForceDirected test data out of the spec

--- src/components/ForceDirectedGraph/ForceDirectedSpec.ts
+++ src/components/ForceDirectedGraph/ForceDirectedSpec.ts
@@ -5,62 +5,6 @@ import {
 } from '../../data/color_palettes'
 import { Mark, Scale, Signal } from 'vega'
 
-// export const TEST_NODES: ForceDirectedGraphNode[] = [
-//   { title: 'Preprint', count: 22357 },
-//   { title: 'Software', count: 136204 },
-//   { title: 'Dataset', count: 7524645 },
-//   { title: 'Organization', count: 97795 },
-//   { title: 'Journal Article', count: 14043135 },
-//   { title: 'People', count: 8718993 }
-// ]
-export const TEST_NODES: ForceDirectedGraphNode[] = [
-  { title: 'Preprint', count: 22 },
-  { title: 'Software', count: 13 },
-  { title: 'Dataset', count: 75 },
-  { title: 'Organization', count: 9 },
-  { title: 'Journal Article', count: 140 },
-  { title: 'People', count: 871 }
-]
-
-export const TEST_LINKS: ForceDirectedGraphLink[] = [
-  { source: 'Preprint', target: 'Software', count: 79 },
-  { source: 'Preprint', target: 'Dataset', count: 682 },
-  { source: 'Preprint', target: 'Journal Article', count: 252 },
-
-  { source: 'Software', target: 'Dataset', count: 19 },
-  { source: 'Software', target: 'People', count: 286 },
-  { source: 'Software', target: 'Organization', count: 2 },
-
-  { source: 'Dataset', target: 'Organization', count: 117 },
-  { source: 'Dataset', target: 'People', count: 546 },
-  { source: 'Dataset', target: 'Journal Article', count: 3876 },
-
-  { source: 'Organization', target: 'Journal Article', count: 17 },
-  { source: 'Organization', target: 'People', count: 39 },
-
-  { source: 'Journal Article', target: 'People', count: 3847 },
-  { source: 'Journal Article', target: 'Journal Article', count: 20 }
-]
-// export const TEST_LINKS: ForceDirectedGraphLink[] = [
-//   { source: 'Preprint', target: 'Software', count: 7992 },
-//   { source: 'Preprint', target: 'Dataset', count: 68292 },
-//   { source: 'Preprint', target: 'Journal Article', count: 25263 },
-//
-//   { source: 'Software', target: 'Dataset', count: 1983 },
-//   { source: 'Software', target: 'People', count: 28612 },
-//   { source: 'Software', target: 'Organization', count: 24 },
-//
-//   { source: 'Dataset', target: 'Organization', count: 11657 },
-//   { source: 'Dataset', target: 'People', count: 546454 },
-//   { source: 'Dataset', target: 'Journal Article', count: 387906 },
-//
-//   { source: 'Organization', target: 'Journal Article', count: 1777 },
-//   { source: 'Organization', target: 'People', count: 3009 },
-//
-//   { source: 'Journal Article', target: 'People', count: 3847926 },
-//   { source: 'Journal Article', target: 'Journal Article', count: 2043502 }
-// ]
-
 export interface ForceDirectedGraphNode {
   title: string
   group?: string
@@ -75,11 +19,7 @@ export interface ForceDirectedGraphLink {
 
 ///// Signals ///////////////////////////////////
 const signals: Signal[] = [
-  {
-    name: 'linkDistance',
-    value: 200
-    // bind: { input: 'range', min: 1, max: 500, step: 1 }
-  },
+  { name: 'linkDistance', value: 200 },
   {
     name: 'selected',
     value: null,
@@ -97,16 +37,8 @@ const signals: Signal[] = [
   // Force specific signals
   { name: 'cx', update: 'width / 2' },
   { name: 'cy', update: 'height / 2' },
-  {
-    name: 'gravityX',
-    value: 0
-    // bind: { input: 'range', min: 0, max: 1 },
-  },
-  {
-    name: 'gravityY',
-    value: 0.2
-    // bind: { input: 'range', min: 0, max: 1 },
-  },
+  { name: 'gravityX', value: 0 },
+  { name: 'gravityY', value: 0.2 },
 
   {
     name: 'fix',
@@ -188,7 +120,6 @@ const nodeMarks: Mark = {
         {
           force: 'collide',
           iterations: 10,
-          // radius: 10
           radius: { expr: 'sqrt(datum.size) / 2' }
         },
         { force: 'nbody', strength: -200 },
