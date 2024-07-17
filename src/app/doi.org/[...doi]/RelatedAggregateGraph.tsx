adde7ce30ffeebd3aae2ff48edc5e6f8ae964535
jrhoads <jrhoads@datacite.org>
2024-07-17 14:52:03 +0200

    Add styling, tooltips, and explanatory text to RelatedAggregateGraph

--- src/app/doi.org/[...doi]/RelatedAggregateGraph.tsx
+++ src/app/doi.org/[...doi]/RelatedAggregateGraph.tsx
@@ -3,6 +3,7 @@ import { Col, Row } from "src/components/Layout";
 import { getRelatedWorksGraph } from 'src/data/queries/relatedWorks'
 import ForceDirectedGraph  from 'src/components/ForceDirectedGraph/ForceDirectedGraph'
 import EmptyChart from 'src/components/EmptyChart/EmptyChart'
+import styles from "./RelatedAggregateGraph.module.scss"
 
 interface Props {
   doi: string
@@ -10,16 +11,26 @@ interface Props {
 
 export default async function RelatedAggregateGraph( {doi}: Props) {
   const data = await getRelatedWorksGraph(doi)
-  const titleText = ""
+  const titleText = "Project Related Works"
   const emptyTitleText = "No related works found"
-  const innerGraph = (data.nodes.length==0) ?
-    <EmptyChart title={emptyTitleText}/> :
-    <ForceDirectedGraph titleText={titleText} nodes={data.nodes} links={data.links} />
+  const helpText = 'The “relatedIdentifier” and “resourceTypeGeneral” fields in the metadata of the project DOI and related work DOIs were used to generate this graph.'
+  const explanitoryText="The network graph visualizes the relationships between different work types in the project. It shows the number of instances of each work type, and hovering over a connection reveals the number of links between any two types."
+  const graphExists = data.nodes.length >1;
+  const innerGraph = (graphExists) ?
+    <ForceDirectedGraph
+        titleText={titleText}
+        nodes={data.nodes}
+        links={data.links}
+        tooltipText={helpText}
+    /> :<EmptyChart title={emptyTitleText}/>
 
   return (<Row>
             <Col mdOffset={3} className="panel panel-transparent">
               <div className="panel-body">
                 {innerGraph}
+                { graphExists &&
+                  <p className={styles.explanitoryText}>{explanitoryText}</p>
+                }
               </div>
             </Col>
           </Row>
