5017340eadf40379f75266d1256d68a9302f844e
jrhoads <jrhoads@datacite.org>
2024-08-14 17:21:30 +0200

    Remove unused imports

--- src/app/doi.org/[...doi]/page.tsx
+++ src/app/doi.org/[...doi]/page.tsx
@@ -4,14 +4,13 @@ import { redirect, notFound } from 'next/navigation'
 import Script from 'next/script'
 import truncate from 'lodash/truncate'
 
-import { rorFromUrl, isProject, isDMP, isAwardGrant } from 'src/utils/helpers'
+import { rorFromUrl, isProject, isDMP } from 'src/utils/helpers'
 
 import apolloClient from 'src/utils/apolloClient/apolloClient'
 import { CROSSREF_FUNDER_GQL } from 'src/data/queries/crossrefFunderQuery'
 import Content from './Content'
 import { DOI_METADATA_QUERY, MetadataQueryData, MetadataQueryVar } from 'src/data/queries/doiQuery'
 import RelatedContent from './RelatedContent'
-import RelatedAggregateGraph from './RelatedAggregateGraph'
 import NetworkGraph from './NetworkGraph'
 import Loading from 'src/components/Loading/Loading'
 
