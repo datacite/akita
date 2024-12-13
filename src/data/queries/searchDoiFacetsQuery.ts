4b19829bbccd525428635036a8948c59cb0f1e10
Bryceson Laing <bryceson.laing@datacite.org>
2024-12-13 09:42:50 -0600

    Replace doi search graphql with rest api (#425)
    * set up react-query
    
    * move functions into helper
    
    * add new search dois hook using REST API
    
    * fix pager color
    
    * split into separate results and facets queries
    
    * use on-demand facets
    
    * add language and registrationAgency to ES query

---
+++ src/data/queries/searchDoiFacetsQuery.ts
@@ -0,0 +1,100 @@
+import { useQuery } from '@tanstack/react-query'
+import type { Works } from 'src/data/types'
+import type { QueryVar } from './searchDoiQuery'
+
+
+const FACETS = [
+  'published',
+  'resourceTypes',
+  'languages',
+  'licenses',
+  'fieldsOfScience',
+  'affiliations',
+  'repositories',
+  'registrationAgencies',
+  'authors',
+  'creatorsAndContributors',
+  // personToWorkTypesMultilevel: []
+]
+
+
+function buildDoiSearchParams(variables: QueryVar): URLSearchParams {
+  const query = variables.query +
+    (variables.language ? ' AND language:' + variables.language : '') +
+    (variables.registrationAgency ? ' AND agency:' + variables.registrationAgency : '')
+
+  const searchParams = new URLSearchParams({
+    query,
+    facets: FACETS.join(','),
+    affiliation: 'false',
+    publisher: 'false',
+    'disable-facets': 'false',
+    include_other_registration_agencies: 'true',
+    'page[size]': '0'
+  })
+
+
+  if (variables.license) searchParams.append('license', variables.license)
+  if (variables.published) searchParams.append('published', variables.published)
+  if (variables.resourceTypeId) searchParams.append('resource-type-id', variables.resourceTypeId)
+  if (variables.fieldOfScience) searchParams.append('field-of-science', variables.fieldOfScience)
+
+  return searchParams
+}
+
+
+function convertToQueryData(json: any): QueryData {
+  const { meta } = json
+
+  // Missing authors, repositories, and multilevel
+  return {
+    works: {
+      published: meta.published,
+      resourceTypes: meta.resourceTypes.slice(0, 10),
+      languages: meta.languages.slice(0, 10),
+      licenses: meta.licenses.slice(0, 10),
+      fieldsOfScience: meta.fieldsOfScience.slice(0, 10),
+      affiliations: meta.affiliations,
+      repositories: [],
+      registrationAgencies: meta.registrationAgencies,
+      authors: [].slice(0, 10),
+      creatorsAndContributors: meta.creatorsAndContributors.slice(0, 10),
+
+      personToWorkTypesMultilevel: []
+    }
+  }
+}
+
+export async function fetchDoisFacets(variables: QueryVar) {
+  try {
+    const options = {
+      method: 'GET',
+      headers: { accept: 'application/vnd.api+json' }
+    }
+    const searchParams = buildDoiSearchParams(variables)
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/dois?${searchParams.toString()}`,
+      options
+    )
+    const json = await res.json()
+
+    const data = convertToQueryData(json)
+    return { data }
+  } catch (error) {
+    return { error }
+  }
+}
+
+export function useSearchDoiFacetsQuery(variables: QueryVar) {
+  // eslint-disable-next-line no-unused-vars
+  const { cursor, ...vars } = variables
+  const { isPending, data, error } = useQuery({ queryKey: ['doiFacetsSearch', vars], queryFn: async () => fetchDoisFacets(variables) })
+
+  return { loading: isPending, data: data?.data, error }
+}
+
+
+export interface QueryData {
+  works: Omit<Works, 'nodes' | 'totalCount' | 'pageInfo'>
+}
--- src/data/queries/searchDoiQuery.ts
+++ src/data/queries/searchDoiQuery.ts
@@ -1,10 +1,78 @@
-import { gql, useQuery } from '@apollo/client'
-import { Works } from 'src/data/types'
+import { gql, useQuery as useGQLQuery } from '@apollo/client'
+import { useQuery } from '@tanstack/react-query'
+import { PageInfo, Works } from 'src/data/types'
 import { workFragment, workConnection } from 'src/data/queries/queryFragments'
+import { mapJsonToWork } from 'src/utils/helpers'
+
+function buildDoiSearchParams(variables: QueryVar): URLSearchParams {
+  const query = variables.query +
+    (variables.language ? ' AND language:' + variables.language : '') +
+    (variables.registrationAgency ? ' AND agency:' + variables.registrationAgency : '')
+
+  const searchParams = new URLSearchParams({
+    query,
+    include: 'client',
+    affiliation: 'false',
+    publisher: 'false',
+    'disable-facets': 'true',
+    include_other_registration_agencies: 'true'
+  })
+
+
+  searchParams.append('page[cursor]', variables.cursor || '1')
+  if (variables.license) searchParams.append('license', variables.license)
+  if (variables.published) searchParams.append('published', variables.published)
+  if (variables.resourceTypeId) searchParams.append('resource-type-id', variables.resourceTypeId)
+  if (variables.fieldOfScience) searchParams.append('field-of-science', variables.fieldOfScience)
+
+  return searchParams
+}
+
+
+function convertToQueryData(json: any): QueryData {
+  const nodes = json.data as any[]
+  const { included, meta, links } = json
+
+  return {
+    works: {
+      totalCount: meta.total,
+      pageInfo: getPageInfo(links),
+      nodes: nodes.map(w => mapJsonToWork(w, included)),
+    }
+  }
+}
+
+export async function fetchDois(variables: QueryVar) {
+  try {
+    const options = {
+      method: 'GET',
+      headers: { accept: 'application/vnd.api+json' }
+    }
+    const searchParams = buildDoiSearchParams(variables)
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/dois?${searchParams.toString()}`,
+      options
+    )
+    const json = await res.json()
+
+    const data = convertToQueryData(json)
+    return { data }
+  } catch (error) {
+    return { error }
+  }
+}
 
 
 export function useSearchDoiQuery(variables: QueryVar) {
-  const { loading, data, error } = useQuery<QueryData, QueryVar>(
+  const { isPending, data, error } = useQuery({ queryKey: ['doiSearch', variables], queryFn: async () => fetchDois(variables) })
+
+  return { loading: isPending, data: data?.data, error }
+}
+
+
+export function useSearchDoiQueryGQL(variables: QueryVar) {
+  const { loading, data, error } = useGQLQuery<QueryData, QueryVar>(
     SEARCH_DOI_QUERY,
     {
       variables,
@@ -50,7 +118,7 @@ export const SEARCH_DOI_QUERY = gql`
 
 
 export interface QueryData {
-  works: Works
+  works: Pick<Works, 'nodes' | 'totalCount' | 'pageInfo'>
 }
 
 export interface QueryVar {
@@ -65,3 +133,12 @@ export interface QueryVar {
 }
 
 
+function getPageInfo(links: { self: string, next?: string }): PageInfo {
+  if (!links.next) return { endCursor: '', hasNextPage: false };
+
+  const url = new URL(links.next);
+  const cursor = url.searchParams.get("page[cursor]")
+  if (!cursor) return { endCursor: '', hasNextPage: false };
+
+  return { endCursor: cursor, hasNextPage: true }
+}
