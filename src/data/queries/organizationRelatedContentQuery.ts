1bd04a86e53045a7fdf5bea59833640504a8d708
Bryceson Laing <bryceson.laing@datacite.org>
2025-01-10 07:59:19 -0600

    replace graphQL with REST API for organization related content (#434)
    * replace graphQL with REST API

--- src/data/queries/organizationRelatedContentQuery.ts
+++ src/data/queries/organizationRelatedContentQuery.ts
@@ -1,9 +1,41 @@
 import { gql, useQuery } from "@apollo/client";
 import { workConnection, workFragment } from "src/data/queries/queryFragments";
-import { QueryData, QueryVar } from "./organizationQuery";
+import { Works } from "src/data/types";
+import { useSearchDoiQuery } from "./searchDoiQuery";
+import { useSearchDoiFacetsQuery } from "./searchDoiFacetsQuery";
 
 
 export function useOrganizationRelatedContentQuery(variables: QueryVar) {
+  const id = 'ror.org/' + variables.id
+  const rorId = `"https://${id}"`
+  const query = `(organization_id:${id} OR affiliation_id:${id} OR related_dmp_organization_id:${id} OR provider.ror_id:${rorId})` + (variables.filterQuery ? ' AND ' + variables.filterQuery : '')
+
+  const vars = { ...variables, query }
+  const results = useSearchDoiQuery(vars)
+  const facets = useSearchDoiFacetsQuery(vars)
+
+  const loading = results.loading || facets.loading;
+  const error = results.error || facets.error;
+
+  if (loading || error) return { loading, data: undefined, error }
+
+
+  const organization = {
+    works: {
+      ...results.data?.works || {} as any,
+      ...facets.data?.works
+    }
+  }
+
+  return {
+    ...results,
+    data: { organization },
+  }
+}
+
+
+
+export function useOrganizationRelatedContentQueryGQL(variables: QueryVar) {
   const { loading, data, error } = useQuery<QueryData, QueryVar>(
     RELATED_CONTENT_QUERY,
     {
@@ -57,4 +89,21 @@ query getRelatedContentQuery(
   ${workFragment}
 `;
 
-export type { QueryData, QueryVar }
+
+export interface QueryVar {
+  id: string
+  filterQuery?: string
+  cursor?: string
+  published?: string
+  resourceTypeId?: string
+  language?: string
+  fieldOfScience?: string
+  license?: string
+  registrationAgency?: string
+}
+
+export interface QueryData {
+  organization: {
+    works: Pick<Works, 'nodes' | 'totalCount' | 'pageInfo'>
+  }
+}
