0870c7dbfa6ee05b0a1162913f58facb7706b057
jrhoads <jrhoads@datacite.org>
2025-01-27 15:13:14 +0100

    "Add titleCase helper function to convert strings to title case"

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -131,6 +131,14 @@ function kebabify(input: string): string {
     .toLowerCase()
 }
 
+export function titleCase(input: string): string {
+  return input
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
 
 
 interface Repository {
