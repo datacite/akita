eff24169523e801b600bd5a39d6e11916cf24f7e
jrhoads <jrhoads@datacite.org>
2025-02-18 15:43:10 +0100

    feat: Validate ROR ID format and catch errors when fetching

--- src/app/ror.org/[rorid]/page.tsx
+++ src/app/ror.org/[rorid]/page.tsx
@@ -11,52 +11,54 @@ interface Props {
     rorid: string
   }
 }
-
+function isValidRORId(rorid: string): boolean {
+  const rorIdPattern = /^0[a-hj-km-np-tv-z|0-9]{6}[0-9]{2}$/
+  return rorIdPattern.test(rorid)
+}
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { rorid } = params
-
-  const { data } = await fetchOrganizationMetadata(rorid)
-
-  if (!data) return {
-    title: '',
-    description: '',
-    openGraph: {
-      title: '',
-      description: '',
-      url: '',
-      images: [{ url: '' }]
-    }
+  if (!isValidRORId(rorid)) {
+    notFound()
   }
 
-  const organization = data.organization
+  try {
+    const { data } = await fetchOrganizationMetadata(rorid)
 
-  const title = organization.name
-    ? 'DataCite Commons: ' + organization.name
-    : 'DataCite Commons: No Name'
-
-
-  const pageUrl =
-    process.env.NEXT_PUBLIC_API_URL === 'https://api.datacite.org'
-      ? 'https://commons.datacite.org/ror.org' + rorid
-      : 'https://commons.stage.datacite.org/ror.org' + rorid
-
-
-  const imageUrl =
-    process.env.NEXT_PUBLIC_API_URL === 'https://api.datacite.org'
-      ? 'https://commons.datacite.org/images/logo.png'
-      : 'https://commons.stage.datacite.org/images/logo.png'
-
-
-  return {
-    title: title,
-
-    openGraph: {
-      title: title,
-      // type: 'organization',
-      url: pageUrl,
-      images: [{ url: imageUrl }]
+    if (!data) {
+      notFound()
     }
+
+    const organization = data.organization
+
+    const title = organization.name
+      ? 'DataCite Commons: ' + organization.name
+      : 'DataCite Commons: No Name'
+
+
+    const pageUrl =
+      process.env.NEXT_PUBLIC_API_URL === 'https://api.datacite.org'
+        ? 'https://commons.datacite.org/ror.org/' + rorid
+        : 'https://commons.stage.datacite.org/ror.org/' + rorid
+
+
+    const imageUrl =
+      process.env.NEXT_PUBLIC_API_URL === 'https://api.datacite.org'
+        ? 'https://commons.datacite.org/images/logo.png'
+        : 'https://commons.stage.datacite.org/images/logo.png'
+
+
+    return {
+      title: title,
+      openGraph: {
+        title: title,
+        // type: 'organization',
+        url: pageUrl,
+        images: [{ url: imageUrl }]
+      }
+    }
+  } catch(error){
+    notFound()
   }
 }
 
@@ -65,12 +67,20 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 export default async function Page({ params }: Props) {
   const { rorid } = params
 
+  // Validate ROR ID format
+  if (!isValidRORId(rorid)) {
+    notFound()
+  }
   // Fetch Organization metadata
-  const { data } = await fetchOrganizationMetadata(rorid)
-  if (!data) notFound()
+  try {
+    const { data } = await fetchOrganizationMetadata(rorid)
+    if (!data) notFound()
 
-  return <>
-    <Content rorid={rorid} />
-    <RelatedContent />
-  </>
+    return <>
+      <Content rorid={rorid} />
+      <RelatedContent />
+    </>
+  } catch (error) {
+    notFound()
+  }
 }
