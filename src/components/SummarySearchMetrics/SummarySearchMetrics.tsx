afa3f73209b7e3a66085c16b1c1aac9302aecf91
jrhoads <jrhoads@datacite.org>
2025-02-19 21:08:16 +0100

    Add loading state to summary search metrics

--- src/components/SummarySearchMetrics/SummarySearchMetrics.tsx
+++ src/components/SummarySearchMetrics/SummarySearchMetrics.tsx
@@ -2,6 +2,30 @@ import React from 'react'
 import { MetricsDisplay } from 'src/components/MetricsDisplay/MetricsDisplay'
 import searchSummaryCounts from 'src/data/queries/searchSummaryQuery'
 import { type QueryVar } from 'src/data/queries/searchDoiQuery'
+import ContentLoader from "react-content-loader"
+
+const StatsLoader = () => (
+  <ContentLoader
+    speed={2}
+    width={700}
+    height={65}
+    viewBox="0 0 700 65"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <rect y="0"   x="0"   rx="5" ry="5" width="18%" height="10%" />
+    <rect y="0"   x="23%" rx="5" ry="5" width="18%" height="10%" />
+    <rect y="0"   x="46%" rx="5" ry="5" width="18%" height="10%" />
+    <rect y="0"   x="69%" rx="5" ry="5" width="18%" height="10%" />
+    <rect y="15%" x="0"   rx="5" ry="5" width="89%" height="75%" />
+  </ContentLoader>
+)
+
+const SummaryStatsLoader = () => (
+  <div className="d-flex" style={{ width: "100%"}}>
+    <StatsLoader />
+  </div>
+)
 
 const links = {
   citations: 'https://support.datacite.org/docs/citations-and-references',
@@ -10,13 +34,13 @@ const links = {
 }
 
 
+
 export default function SummarySearchMetrics(variables: QueryVar) {
 
   const { data, error, isLoading } = searchSummaryCounts(variables)
 
-  if (isLoading) return <div>Loading metrics...</div>
-  if (error) return <div>Error loading metrics</div>
-  if (!data) return null
+  if (isLoading) return <SummaryStatsLoader />
+  if (!data || error) return null
 
   return <MetricsDisplay counts={data} links={links} />
 }
