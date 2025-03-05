c46988adc291d794b299c89201ee16740531ca85
Bryceson Laing <bryceson.laing@datacite.org>
2025-03-05 16:23:59 -0500

    don't fetch works if clientId isn't set

--- src/data/queries/repositoryRelatedContentQuery.ts
+++ src/data/queries/repositoryRelatedContentQuery.ts
@@ -1,6 +1,7 @@
 import { useSearchDoiFacetsQuery } from './searchDoiFacetsQuery'
 
 export function useRepositoryRelatedContent(id: string) {
+  if (!id) return { data: undefined }
   const vars = { clientId: id }
   return useSearchDoiFacetsQuery(vars)
 }
