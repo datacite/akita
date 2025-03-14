9fef024117890e30d294352bce4255746a7197eb
Bryceson Laing <bryceson.laing@datacite.org>
2025-03-14 13:56:00 -0200

    migrate organization related works report to REST API

--- src/app/download-reports/ror/related-works/route.ts
+++ src/app/download-reports/ror/related-works/route.ts
@@ -1,74 +1,21 @@
 import { type NextRequest } from 'next/server'
-import { gql } from '@apollo/client';
-import apolloClient from 'src/utils/apolloClient/apolloClient'
 import { stringify } from 'csv-stringify/sync'
-
-const QUERY = gql`
-  query relatedWorksRorQuery(
-    $id: ID
-    $gridId: ID
-    $crossrefFunderId: ID
-    $cursor: String
-    $filterQuery: String
-    $published: String
-    $resourceTypeId: String
-    $fieldOfScience: String
-    $language: String
-    $license: String
-    $registrationAgency: String
-  ) {
-    organization(
-      id: $id
-      gridId: $gridId
-      crossrefFunderId: $crossrefFunderId
-    ) {
-      works(
-        first: 200
-        after: $cursor
-        query: $filterQuery
-        published: $published
-        resourceTypeId: $resourceTypeId
-        fieldOfScience: $fieldOfScience
-        language: $language
-        license: $license
-        registrationAgency: $registrationAgency
-      ) {
-        nodes {
-          ...WorkFragment
-        }
-      }
-    }
-  }
-  
-  fragment WorkFragment on Work {
-    titles {
-      title
-    }
-    descriptions {
-      description
-      descriptionType
-    }
-    types {
-      resourceTypeGeneral
-      resourceType
-    }
-    doi
-    formattedCitation(style: "apa", locale: "en-US", format: text)
-    publicationYear
-  }
-`
+import { fetchDois, fetchDoisCitations } from 'src/data/queries/searchDoiQuery';
 
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const variables = Object.fromEntries(searchParams)
 
-  const { data } = await apolloClient.query({
-    query: QUERY,
-    variables: variables
-  })
+  const [responseWorks, citations] = await Promise.all([
+    fetchDois(variables, 200),
+    fetchDoisCitations(variables, 200),
+  ]);
 
-  const sortedData = [...data.organization.works.nodes].sort((a, b) => b.publicationYear - a.publicationYear)
+  const works = responseWorks.data.works.nodes.map((work, index) => ({ ...work, formattedCitation: citations[index] }));
+
+
+  const sortedData = [...works].sort((a, b) => b.publicationYear - a.publicationYear)
 
   const csv = stringify(sortedData, {
     header: true,
