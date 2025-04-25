fa293b6bd6fca4aa690ebd8cffbd574312691b76
jrhoads <jrhoads@datacite.org>
2025-04-25 17:50:12 +0200

    Configure API proxy to local backend and add run scripts

--- next.config.js
+++ next.config.js
@@ -37,6 +37,17 @@ module.exports = withSourceMaps({
   images: {
     domains: ['img.shields.io']
   },
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5328/api/:path*'
+            : '/api/',
+      },
+    ]
+  },
   staticPageGenerationTimeout: 220,
   webpack: (config, options) => {
     // import yaml files
