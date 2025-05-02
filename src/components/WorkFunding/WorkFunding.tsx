f51f098662bbefb7b211aef7b3e93449c30248a4
codycooperross <50597551+codycooperross@users.noreply.github.com>
2025-05-02 11:37:27 +0200

    Adds support for ROR identifiers when linking to a funder from a WorkFunding component on Work pages

--- src/components/WorkFunding/WorkFunding.tsx
+++ src/components/WorkFunding/WorkFunding.tsx
@@ -2,31 +2,36 @@ import React from 'react'
 import Link from 'next/link'
 
 import { FundingReference } from 'src/data/types'
-import { doiFromUrl } from '../../utils/helpers'
+import { doiFromUrl, rorFromUrl } from '../../utils/helpers'
 
 type Props = {
   funding: FundingReference
 }
 
 const WorkFunding: React.FunctionComponent<Props> = ({ funding }) => {
-  const showAwardLink = funding.funderIdentifier &&
-    funding.funderIdentifier.startsWith('https://doi.org/10.13039') &&
+  const funderLink = (() => {
+    if (funding.funderIdentifier?.startsWith('https://doi.org/10.13039')) {
+      return '/doi.org' + doiFromUrl(funding.funderIdentifier);
+    }
+    if (funding.funderIdentifier?.startsWith('https://ror.org')) {
+      return '/ror.org' + rorFromUrl(funding.funderIdentifier);
+    }
+    return null;
+  })();
+  const showAwardLink = (
+    funding.funderIdentifier &&
+    funderLink &&
     funding.awardNumber
+  )
 
-  const funder = () => {
-    if (
-      funding.funderIdentifier &&
-      funding.funderIdentifier.startsWith('https://doi.org/10.13039')
-    )
-      return (
-        <h4 className="work">
-          <Link prefetch={false} href={'/doi.org' + doiFromUrl(funding.funderIdentifier)}>
-            {funding.funderName}
-          </Link>
-        </h4>
-      )
-
-    return <h4 className="work">{funding.funderName}</h4>
+  const funder = () => {    
+    return funderLink ?
+      <h4 className="work">
+        <Link prefetch={false} href={funderLink}>
+          {funding.funderName}
+        </Link>
+      </h4> : 
+      <h4 className="work">{funding.funderName}</h4>
   }
 
   return (
@@ -37,7 +42,7 @@ const WorkFunding: React.FunctionComponent<Props> = ({ funding }) => {
       )}
       {showAwardLink && (
         <div className="award">
-          <Link prefetch={false} href={'/doi.org' + doiFromUrl(funding.funderIdentifier || '') + '?query=fundingReferences.awardNumber:(' + funding.awardNumber + ')'}>
+          <Link prefetch={false} href={funderLink && funderLink + '?filterQuery=fundingReferences.awardNumber:(' + funding.awardNumber + ')'}>
             {funding.awardNumber}
           </Link>
         </div>
