ecb903100687b620d8245789d7d4575bc8f11f22
codycooperross <50597551+codycooperross@users.noreply.github.com>
2025-10-30 12:32:46 +0100

    Fix comment

--- src/components/WorksListing/WorksListing.tsx
+++ src/components/WorksListing/WorksListing.tsx
@@ -37,7 +37,7 @@ export default function WorksListing({
   showAnalytics,
   connectionTypesCounts,
   showSankey,
-  // sankeyTitle = 'Contributions to Related Works',
+  sankeyTitle = 'Contributions to Related Works',
   showClaimStatus,
   loading,
   loadingFacets = false,
