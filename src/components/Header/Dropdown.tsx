2a19b55d32d157c58d353f8474e27cff167974f3
jrhoads <jrhoads@datacite.org>
2025-12-04 18:38:08 +0100

    test: improve session handling and test cookie logging

--- src/components/Header/Dropdown.tsx
+++ src/components/Header/Dropdown.tsx
@@ -5,7 +5,8 @@ import { useSession } from 'src/utils/session'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 
 export default function UserDropdown({ children }: PropsWithChildren) {
-  const { user } = useSession()
+  const { user, loading } = useSession()
+  if (loading) return null // Or return a loading skeleton
   if (!user) throw new Error("User not signed in")
 
   return <NavDropdown title={user.name} className="my-4" id="basic-nav-dropdown">
