3070469605d347e31696091165875b47712e883e
jrhoads <jrhoads@datacite.org>
2025-12-04 14:35:33 +0100

    feat: Update ApolloProvider to accept nullable token and make token retrieval async

--- src/utils/apolloClient/provider.tsx
+++ src/utils/apolloClient/provider.tsx
@@ -2,9 +2,9 @@ import React, { PropsWithChildren } from 'react'
 import apolloClientBuilder from 'src/utils/apolloClient/builder'
 import { ApolloNextAppProvider } from "@apollo/experimental-nextjs-app-support";
 
-export default function ApolloProvider({ token, children }: PropsWithChildren<{ token: string }>) {
+export default function ApolloProvider({ token, children }: PropsWithChildren<{ token: string | null }>) {
   return (
-    <ApolloNextAppProvider makeClient={() => apolloClientBuilder(() => token)}>
+    <ApolloNextAppProvider makeClient={() => apolloClientBuilder(async () => token)}>
       {children}
     </ApolloNextAppProvider>
   );
