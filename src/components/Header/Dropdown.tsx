d3389b60190129ac2b4a765b20272a5f20fcbed8
jrhoads <jrhoads@datacite.org>
2025-12-04 14:19:34 +0100

    refactor: Use useSession hook in multiple components

--- src/components/Header/Dropdown.tsx
+++ src/components/Header/Dropdown.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import React, { PropsWithChildren } from 'react'
-import { session } from 'src/utils/session'
+import { useSession } from 'src/utils/session'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 
 export default function UserDropdown({ children }: PropsWithChildren) {
-  const user = session()
+  const { user } = useSession()
   if (!user) throw new Error("User not signed in")
 
   return <NavDropdown title={user.name} className="my-4" id="basic-nav-dropdown">
