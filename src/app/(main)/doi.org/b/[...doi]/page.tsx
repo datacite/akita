7d5e9f479bd56b3cfd00fb860464343c67b9507b
jrhoads <jrhoads@datacite.org>
2026-01-05 13:07:45 +0100

    refactor: remove DOI data fetching and simplify Page component in doi.org route

--- src/app/(main)/doi.org/b/[...doi]/page.tsx
+++ src/app/(main)/doi.org/b/[...doi]/page.tsx
@@ -1,24 +1,9 @@
 import React from 'react'
-import { notFound } from 'next/navigation'
-import { fetchDoi } from 'src/data/queries/doiQuery'
 import RelatedContent from './RelatedContentBeta'
 
 
-interface Props {
-  params: {
-    doi: string[]
-  }
-}
-
-export default async function Page({ params }: Props) {
-  const doi = decodeURIComponent(params.doi.join('/'))
-
-
-  // Fetch DOI metadata
-  const { data } = await fetchDoi(doi)
-  if (!data) notFound()
-
+export default function Page() {
   return <>
-    <RelatedContent work={data.work}/>
+    <RelatedContent/>
   </>
 }
