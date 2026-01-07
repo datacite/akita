8f2925ce18b7b5939ca80bff101bbeb6a9969647
jrhoads <jrhoads@datacite.org>
2026-01-07 16:56:13 +0100

    refactor: use dynamic title for RelatedContentBeta component

--- src/app/(main)/doi.org/b/[...doi]/RelatedContentBeta.tsx
+++ src/app/(main)/doi.org/b/[...doi]/RelatedContentBeta.tsx
@@ -109,7 +109,7 @@ export default function RelatedContent() {
     <Container fluid>
       <Row>
         <Col md={{ offset: 3 }}>
-          <h3 className="member-results" id="title">BETA - Related Works</h3>
+          <h3 className="member-results" id="title">BETA - {displayedConnectionTitle}</h3>
         </Col>
       </Row>
       <Row>
