b1aa3a059e4ffff9033f2a97e95b675fca22c16c
jrhoads <jrhoads@datacite.org>
2026-02-05 16:47:46 +0100

    feat: add InfoTooltip component with react-bootstrap and font-awesome

---
+++ src/components/InfoTooltip/InfoTooltip.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
+
+interface InfoTooltipProps {
+  text: string
+}
+
+export const InfoTooltip = ({ text }: InfoTooltipProps) => (
+  <OverlayTrigger
+    placement="top"
+    overlay={<Tooltip id="infoTooltip">{text}</Tooltip>}
+  >
+    <span
+      onClick={(e) => e.stopPropagation()}
+      className="ms-2"
+      style={{ cursor: 'help' }}
+      role="button"
+      tabIndex={0}
+      aria-label={text}
+    >
+      <FontAwesomeIcon icon={faQuestionCircle} aria-hidden="true" />
+    </span>
+  </OverlayTrigger>
+)
