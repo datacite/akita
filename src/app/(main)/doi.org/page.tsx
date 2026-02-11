e3519f0de452bb60346296c5fc4f2ce6eb69aabc
jrhoads <jrhoads@datacite.org>
2026-02-11 11:19:20 +0100

    refactor: update import syntax and refine type safety in getQueryVariables

--- src/app/(main)/doi.org/page.tsx
+++ src/app/(main)/doi.org/page.tsx
@@ -5,12 +5,14 @@ import { useSearchParams } from 'next/navigation'
 
 import SearchWork from 'src/components/SearchWork/SearchWork'
 import { WorksExampleText } from 'src/components/ExampleText/ExampleText'
-import mapSearchParams, { type SearchParams } from './mapSearchParams'
+import { default as mapSearchParams, type SearchParams } from './mapSearchParams'
+import { type QueryVar } from 'src/data/queries/searchDoiQuery'
 
 
-function getQueryVariables(searchParams: URLSearchParams): SearchParams {
-  return mapSearchParams(Object.fromEntries(searchParams.entries()) as any)
+function getQueryVariables(searchParams: URLSearchParams): QueryVar {
+  return mapSearchParams(Object.fromEntries(searchParams.entries()) as SearchParams)
 }
+
 export default function SearchDoiPage() {
   const searchParams = useSearchParams()
   const variables = getQueryVariables(searchParams)
