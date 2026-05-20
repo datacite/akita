ff68e04a441b56b847c80d0502be1bd6433876b0
jrhoads <jrhoads@datacite.org>
2026-05-20 09:59:52 +0200

    refactor: Use RORV2Client to fetch crossref funder ID

--- src/data/queries/crossrefFunderQuery.ts
+++ src/data/queries/crossrefFunderQuery.ts
@@ -1,20 +1,17 @@
-import { gql } from '@apollo/client'
-import apolloClient from 'src/utils/apolloClient/apolloClient'
+import { RORV2Client } from 'src/data/clients/ror-v2-client'
+
+const rorClient = new RORV2Client()
+const FUNDREF_PREFIX = '10.13039/'
 
 export async function fetchCrossrefFunder(crossrefFunderId: string) {
-  const { data } = await apolloClient.query({
-    query: CROSSREF_FUNDER_GQL,
-    variables: { crossrefFunderId },
-    errorPolicy: 'all'
+  const fundrefId = crossrefFunderId.startsWith(FUNDREF_PREFIX)
+    ? crossrefFunderId.slice(FUNDREF_PREFIX.length)
+    : crossrefFunderId
+
+  const response = await rorClient.searchOrganizations({
+    queryAdvanced: `external_ids.all:${fundrefId}`,
   })
 
-  return { data }
+  const rorId = response.items[0]?.id ?? null
+  return { data: rorId ? { organization: { id: rorId } } : null }
 }
-
-export const CROSSREF_FUNDER_GQL = gql`
-  query getOrganizationQuery($crossrefFunderId: ID) {
-    organization(crossrefFunderId: $crossrefFunderId) {
-      id
-    }
-  }
-`
